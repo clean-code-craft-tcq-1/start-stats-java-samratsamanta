b3427b284a439a50dff72ca8d6ecc4dba9f1890f
samratsamanta <samratsamanta7@gmail.com>
2021-01-24 12:37:41 +0530

    Test commit

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class Statistics 
 {
     public static Stats getStatistics(List<___> numbers) {
-        //implement the computation of statistics here
+        Stats.average=0.1;
+        Stats.max=0.2;
+        Stats.min=0.3;
     }
 }
