ce3ea20c7de689956344837bd92e1cb1a5d4d01d
samratsamanta <samratsamanta7@gmail.com>
2021-01-24 12:27:38 +0530

    Update Statistics.java

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -4,7 +4,22 @@ import java.util.List;
 
 public class Statistics 
 {
+    class Stats{
+         public Float average,max,min;
+    }
+   
     public static Stats getStatistics(List<___> numbers) {
-        //implement the computation of statistics here
+        Stat.average=avg(numbers);
+        Stat.max=maximun(numbers);
+        Stat.min=minimum(numbers);        
+    }
+    private static Float avg(List<___> numbers){
+        return 0.1;
+    }
+    private static Float maximun(List<___> numbers){
+        return 0.1;
+    }
+    private static Float minimum(List<___> numbers){
+        return 0.1;
     }
 }
